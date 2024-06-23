8faabd473410d8d26171dce784cfb6a6dad44f59
Eduardo <eduardopaessout@gmail.com>
2024-06-23 01:53:43 -0400

    Inicialização da API e rota GET

--- src/server.js
+++ src/server.js
@@ -5,3 +5,11 @@ const port = 3000;
 const morganBody = require('morgan-body');
 const fs = require('fs');
 const path = require('path');
+
+app.listen(port, () => {
+    console.log(`Rodando na porta ${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
